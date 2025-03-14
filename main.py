e53f93fe000ef8f6da3915d3d9a8603ac0b2b4bf
deno997 <denismuheljic42@gmail.com>
2025-03-14 21:11:47 +0100

    Promjenuta kolicina i obrisani prikaz proizvoda i ukupna vrednost

--- main.py
+++ main.py
@@ -6,18 +6,10 @@ if __name__ == "__main__":
     manager = ProductManager()
     
     # Dodavanje proizvoda
-    product1 = Product("Laptop", 1200, 3)
-    product2 = Product("Phone", 800, 5)
-    product3 = Product("Tablet", 500, 7)
+    product1 = Product("Laptop", 1200, 7)
+    product2 = Product("Phone", 800, 9)
+    product3 = Product("Tablet", 500, 4)
     
     manager.add_product(product1)
     manager.add_product(product2)
     manager.add_product(product3)
-    
-    # Prikaz svih proizvoda
-    print("Lista proizvoda:")
-    manager.display_products()
-    
-    # Prikaz ukupne vrednosti inventara
-    print("Ukupna vrednost inventara:")
-    manager.total_value()
