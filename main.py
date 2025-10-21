5432014c4219eabe70239688a882f23089f6dcd3
deno997 <denismuheljic42@gmail.com>
2025-10-21 18:59:22 +0200

    Obrisan kod za prikazivanje proizvoda i ukupne vrijednosti

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ def main():
     manager = ProductManager()
 
 #Dodavanje proizvoda
-    p1 = Product("Hleb", 80, 20)
-    p2 = Product("Mleko", 120, 15)
-    p3 = Product("Jaja", 15, 30)
-    p4 = Product("Sir", 500, 5)
+    p1 = Product("Hleb", 80, 40)
+    p2 = Product("Mleko", 120, 25)
+    p3 = Product("Jaja", 15, 35)
+    p4 = Product("Sir", 500, 8)
 
     manager.add_product(p1)
     manager.add_product(p2)
     manager.add_product(p3)
     manager.add_product(p4)
-
-#Prikaz svih proizvoda
-    manager.show_all_products()
-
-#Prikaz ukupne vrednosti svih proizvoda
-    manager.total_value()
-
+    
 if __name__ == "__main__":
     main()
